959335d3e0e571f6df78f13b89392779ca2b0ed8
Makrand Gupta <accounts@makrand.io>
2022-11-05 20:35:58 -0400

    setup basic header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { MantineProvider, Text, Button, Stack } from "@mantine/core";
+import { Header } from "./components/header";
+import { links } from "./components/header/links";
 import { theme } from "./theme";
 
 export default function App() {
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <Header links={links} />
       <Stack align="center" mt={50}>
         <Text size="xl" weight={500}>
           Welcome to Mantine!
---
+++ src/components/header/links.tsx
@@ -0,0 +1,23 @@
+export type Link = {
+  label: string;
+  link: string;
+};
+
+export const links: Link[] = [
+  {
+    label: "Home",
+    link: "/home",
+  },
+  {
+    label: "About",
+    link: "/about",
+  },
+  {
+    label: "Services",
+    link: "/services",
+  },
+  {
+    label: "Contact",
+    link: "/contact",
+  },
+];
