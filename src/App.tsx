ced4f9b948be86544eeaccf5cea32f6ba41650ae
Makrand Gupta <accounts@makrand.io>
2022-11-05 20:36:16 -0400

    remove starter code

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,6 @@ export default function App() {
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
       <Header links={links} />
-      <Stack align="center" mt={50}>
-        <Text size="xl" weight={500}>
-          Welcome to Mantine!
-        </Text>
-        <Button>Click the button</Button>
-      </Stack>
     </MantineProvider>
   );
 }
